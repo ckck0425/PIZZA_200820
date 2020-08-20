32ed2a591a35b75a90b32992b3631fb00a6e5013
yugyeong ko <ckck0425ko@gmail.com>
2020-08-20 22:03:42 +0900

    가게 상세정보화면 추가

---
+++ app/src/main/java/kr/co/tjoeun/pizza_200820/Fragments/ViewStoreDetailActivity.kt
@@ -0,0 +1,12 @@
+package kr.co.tjoeun.pizza_200820.Fragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewStoreDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_store_detail)
+    }
+}
